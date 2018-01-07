f6440a0472df3aa0804a56cbee3fe6f91961be04
Jan Martijn van der Werf <jm.vanderwerf@znck.nl>
2018-01-07 14:24:48 +0100

    Basic structure for command line options

--- src/fabricad.php
+++ src/fabricad.php
@@ -21,15 +21,95 @@ spl_autoload_register(function ($name) {
 });
 
 
-FaBriCAD::main($_SERVER);
+FaBriCAD::main($_SERVER['argv']);
 
 
 class FaBriCAD
 {
     
+    /**
+     * 
+     * @var \jmw\fabricad\config\Project
+     */
+    private $project = null;
+    
+    private $config = array('in' => '', 'out' => []);
+    
     public static function main($args = array())
     {
-        $c = new \jmw\fabricad\shapes\Rectangle();
+        $f = new FaBriCAD();
+        $status = $f->run($args);
+        
+        exit($status);
+    }
+       
+    
+    private function run($args = array()): int
+    {
+        $this->config = array('in' => '', 'out' => []);
+        
+        if (!is_array($args) || count($args) < 2 || in_array('--help', $args)) {
+            echo $this->returnHelp($args[0]);
+            return 1;
+        }
+        
+        $i = 0;
+        $cmd = '';
+        while($i < count($args)) {
+            if(substr($args[$i],0,1) == '-') {
+                // it is a command!
+                $cmd = trim(substr($args[$i], 1));
+                $this->register($cmd);
+            } else {
+                $this->register($cmd, $args[$i]);
+            }
+            $i++;
+        }
+        
+        foreach($this->config['out'] as $type => $file) {
+            if (empty($file)) {
+                $this->config['out'][$type] = pathinfo($this->config['in'], PATHINFO_FILENAME).".".$type;
+            }
+        }
+        
+        // var_dump($this->config);
+        
+        return 0;
+    }
+    
+    private function register(string $cmd, $filename = '') {
+        switch(substr($cmd, 0, 1)) {
+            case 'I':
+                $this->config['in'] = $filename;
+                break;
+            case 'T':
+                $this->config['out'][substr($cmd,1)] = $filename;
+                break;
+        }
+    }
+    
+    private function returnHelp($progname = ''): string
+    {
+        if (empty($progname)) {
+            $progname = basename(__FILE__);
+        }
+        
+        $str = 'FaBriCAD is a program that creates blueprints in several output formats.';
+        $str .= "\n\n";
+        $str .= 'command line options: '."\n";
+        $str .= "\t -I <project-file>\twith <project-file> the config file\n";
+        $str .= "\t -T<type> <outputfile>\twith <type> the output type and\n";
+        $str .= "\t\t\t\t<outputfile> the file to write to.\n";
+        // $str .= "\t\t\t\tIf no output file is given, STD_OUT is used\n";
+        $str .= "\t --help\t\t\toutputs this help message\n\n\n";
+        
+        $str .= "Currently supported output types:\n";
+        $str .= "\n";
+        $str .= "For example: \n\n\t".$progname." -I ../examples/shed.fabricad -Tsvg shed.svg -Tdxf shed.dxf\n\n";
+        $str .= "will output an SVG drawing to shed.svg, and a DXF drawing to shed.dxf.\n\n\n";
+        $str .= "FaBriCAD (c) 2018, Jan Martijn van der Werf\n\n";
+        
+        return $str;
     }
     
 }
